512af64c16af13a5c5b299f522563606f6a421b2
saskyafernandez <fernandezsaskya@gmail.com>
2022-11-07 14:58:43 +0100

    changes Todo List like Delet Task And Edit Task

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var tbodySelector = document.querySelector("tbody");
 
 
 function addRow(title, deadline, status) {
-  tbodySelector.innerHTML += '<tr id="'+ title +'"><td class="titleTask"><p>' + title + '</p></td><td class="deadlineTask" id="deadlineTask' + i + '"><p>' + deadline + '</p></td><td class="statusTask"><p>' + status + '</p></td><td class="deleteTask">x</td></tr>';
+  tbodySelector.innerHTML += '<tr id="'+ title +'"><td class="titleTask"><p>' + title + '</p></td><td class="deadlineTask" id="deadlineTask' + i + '"><p>' + deadline + '</p></td><td class="statusTask"><p>' + status + '</p></td><td class="editTask" data-toggle="modal" data-target="#editModal">Edit</td><td class="deleteTask">x</td></tr>';
   i++;
   console.log(i);
 }
@@ -25,6 +25,7 @@ document.getElementById('submitTask').addEventListener('click', (e) => {
   var emptyTitle = document.getElementById("emptyTitle");
   var emptyDeadline = document.getElementById("emptydeadline");
   var verifySameTask = false
+  
 
   for (const child of tbodySelector.children) {
     if (child.id == titleTask && child.children[1].textContent.trim() == (deadlineTask.trim())) {
@@ -54,21 +55,56 @@ document.getElementById('submitTask').addEventListener('click', (e) => {
     modalCenter.setAttribute('aria-hidden', 'true');
     document.getElementById('submitTask').setAttribute('data-dismiss', 'modal');
     addRow(titleTask, deadlineTask, textstatusTask);
-
-    var xDeleteTask = document.querySelectorAll('.deleteTask');
-    xDeleteTask.forEach(ele => {
-      console.log('delete', ele);
+    var status = document.querySelectorAll('.statusTask')
+    console.log(status);
+    status.forEach(element => {
+      console.log(element.textContent.trim());
+      // text-decoration: line-through;
+      if (element.textContent.trim() == 'To do') {
+        element.children[0].style.cssText = "color:orangered!important;";
+        console.log("todo");
+      }else if (element.textContent.trim() == 'Pending') {
+        console.log("Pending");
+        element.children[0].style.cssText = "color:orange!important;";
+  
+      }else if (element.textContent.trim() == 'Done') {
+        console.log("Done");
+        element.children[0].style.cssText = "color:green!important;";
+      }else{
+        console.log("error");
+      }
+    });
+    var editTask = document.querySelectorAll('.editTask');
+    editTask.forEach(ele => {
       ele.addEventListener('click', (e) => {
-        console.log("click", e);
         var tdRow = document.getElementById(titleTask);
         var td = e.target.parentElement;
         var tr = td.parentNode;
-        console.log(td);
-        console.log(tr);
+        document.getElementById('titleInputEditTask').value = td.children[0].textContent;
+        document.getElementById('deadlineInputEditTask').value = td.children[1].textContent;
+        if (td.children[2].textContent == "To do") {
+          document.getElementById('statusSelecteEditTask').value = "0";
+        }else if (td.children[2].textContent == "Pending") {
+          document.getElementById('statusSelecteEditTask').value = "1";
+        }else if (td.children[2].textContent == "Done") {
+          document.getElementById('statusSelecteEditTask').value = "2";
+        }else{
+          alert('Error of Status')
+        }
+      });
+    });
+
+
+    var xDeleteTask = document.querySelectorAll('.deleteTask');
+    xDeleteTask.forEach(ele => {
+      ele.addEventListener('click', (e) => {
+        var tdRow = document.getElementById(titleTask);
+        var td = e.target.parentElement;
+        var tr = td.parentNode;
         tr.removeChild(td);
       });
-      console.log('after delete', ele);
     });
+    
     emptyTitle.style.cssText = "display: none;";
     emptyDeadline.style.cssText = "display: none;";
     document.getElementById('titleInputTask').value = "";
@@ -77,5 +113,8 @@ document.getElementById('submitTask').addEventListener('click', (e) => {
 
   }
 
+
+});
+document.getElementById('submitEditTask').addEventListener('click', (e) => {
 
 });
